2f59027d7da4b30cebaae1cf714061025b7b782c
asanheyaa <122150617+asanheyaa@users.noreply.github.com>
2026-09-18 07:26:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// burger-menu
+// const burgerMenu = document.querySelector('.burger-menu');
+// burgerMenu.addEventListener('click', (e) => {
+// 	burgerMenu.classList.toggle('_active');
+// 	document.querySelector('.header-menu').classList.toggle('_active');
+// 	document.body.classList.toggle('_lock');
+// });
